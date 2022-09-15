395826405669d3763db496d02c13b65b2f1858f9
Jitendrayadav7999 <110589302+Jitendrayadav7999@users.noreply.github.com>
2022-09-15 22:30:11 +0530

    Update internCreatValidation.js

--- src/dataValidation/internCreatValidation.js
+++ src/dataValidation/internCreatValidation.js
@@ -91,7 +91,7 @@ const isValidMobile = function (mobile) {
         let regex1 = /^((0091)|(\+91)|0?)[6789]{1}\d{9}$/
 
         if (!(regex1.test(mobile))) {
-            return "mobile number should be start 6 or  10 digits."
+            return "mobile number should be start 6 to 9 or  10 digits."
         }
         //------------------------------------> (END FUNCTION ) <------------------------------//
 
